5b1aa28cc631e2a21d22f78771ae65de83bab13b
hunterabraham <41166326+hunterabraham@users.noreply.github.com>
2019-07-01 16:37:27 -0400

    Update FindThresholdImageQuick.py

--- Python/FindThresholdImageQuick.py
+++ Python/FindThresholdImageQuick.py
@@ -4,16 +4,16 @@ import sys
 import math
 import time
 
-save_folder = "Images/" # Initialize save path
+save_folder = "/home/schoen/Documents/ImageProcessPython/Images/" # Initialize save path
 
 image = cv2.imread(save_folder + "Picture 1.jpg") # Read in sample image
 print(image.shape) #Print it's shape
 
 width = image.shape[1] #Find width, height of image
 height = image.shape[0]
-sigma_multiplier = 3 # number of standard deviations for thresholding
+sigma_multiple = 3 # number of standard deviations for thresholding
 
-img_dev = np.empty((width, height)) # Create empty array to hold threshold values
+img_dev = np.empty((height, width)) # Create empty array to hold threshold values
 row = 0
 pixel = 0
 images = [] # Create array to hold images\
@@ -23,21 +23,21 @@ for i in range(0, num_images):
 	images.append(temp_img) # Add each image to images array
 	print("Adding image " + str(i))
 times = []
-try:
-	for i in range(0, (width - 1)): #For each row
-		t0 = time.time()
-		for j in range(0, (height - 1)): # For each pixel in that row
 
-			print("Row: " + str(j))
-			print("Col: " + str(i))
+try:
+	for i in range(0, height - 1): #For each row
+		print("Col: " + str(i))
+		t0 = time.time()
+		for j in range(0, width - 1): # For each pixel in that row
 			img_data = np.empty(num_images) # Create empty array to hold pixel values for each pixel
 			val = 0
 			for pic in images: #For each picture in images, 
-				img_data[val] = pic[j][i][0] #Add value to image data array
+				img_data[val] = pic[i, j, 0] #Add value to image data array
 				val += 1 # increment index
 			# Set threshold to the mean value plus the deviation
-			img_dev[j][i] = img_data.mean() + (img_data.std() * sigma_multiplier) # Set threshold of each pixel to mean pixel value plus 3 std. deviations
-			print("Standard deviation of " + str(img_data.std()) + " added at (" + str(i) + ", " + str(j))
+			standard_dev = img_data.std() * sigma_multiple
+			mean = img_data.mean()
+			img_dev[i,j] = mean + standard_dev # Set threshold of each pixel to mean pixel value plus 3 std. deviations
 		t1 = time.time()
 		times.append(t1 - t0)
 
