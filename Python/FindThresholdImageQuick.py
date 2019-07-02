a1f30b948f02ea826a9b5583b4b987ad1dc91c86
hunterabraham <41166326+hunterabraham@users.noreply.github.com>
2019-07-02 12:56:51 -0400

    Update FindThresholdImageQuick.py

--- Python/FindThresholdImageQuick.py
+++ Python/FindThresholdImageQuick.py
@@ -1,56 +1,39 @@
 import numpy as np
 import cv2
-import sys
-import math
 import time
 
-save_folder = "/home/schoen/Documents/ImageProcessPython/Images/" # Initialize save path
 
-image = cv2.imread(save_folder + "Picture 1.jpg") # Read in sample image
-print(image.shape) #Print it's shape
-
-width = image.shape[1] #Find width, height of image
+t0 = time.time()
+save_folder = "C:\\Users\\habraha1\\Documents\\Images\\"
+print(cv2.__version__)
+image = cv2.imread(save_folder + "Picture 1.jpg")
+print(image.shape)
+width = image.shape[1]
 height = image.shape[0]
-sigma_multiple = 3 # number of standard deviations for thresholding
-
-img_dev = np.empty((height, width)) # Create empty array to hold threshold values
-row = 0
-pixel = 0
-images = [] # Create array to hold images\
-num_images = 10 #Find number of images you want to threshold with
-for i in range(0, num_images):
-	temp_img = cv2.imread(save_folder + "Picture " + str(i + 1) + ".jpg") # Read in each image
-	images.append(temp_img) # Add each image to images array
-	print("Adding image " + str(i))
-times = []
-
+sigma_multiple = 3
+img_dev = np.empty((height, width))
+NUM_IMAGES = 200
+images = np.empty((NUM_IMAGES, height, width), dtype="uint8")
+for i in range(0, NUM_IMAGES):
+    temp_img = cv2.imread(save_folder + "Picture " + str(i) + ".jpg")
+    temp_img = temp_img[:, :, 0]
+    images[i] = temp_img
+    print("Adding image " + str(i) + "...")
+print("Images shape: " + str(images.shape))
 try:
-	for i in range(0, height - 1): #For each row
-		print("Col: " + str(i))
-		t0 = time.time()
-		for j in range(0, width - 1): # For each pixel in that row
-			img_data = np.empty(num_images) # Create empty array to hold pixel values for each pixel
-			val = 0
-			for pic in images: #For each picture in images, 
-				img_data[val] = pic[i, j, 0] #Add value to image data array
-				val += 1 # increment index
-			# Set threshold to the mean value plus the deviation
-			standard_dev = img_data.std() * sigma_multiple
-			mean = img_data.mean()
-			img_dev[i,j] = mean + standard_dev # Set threshold of each pixel to mean pixel value plus 3 std. deviations
-		t1 = time.time()
-		times.append(t1 - t0)
+    print("Finding variance...")
+    y = np.var(images, axis=0, dtype=np.dtype("uint16"))
+    print("Finding square roots...")
+    y = np.sqrt(y, dtype="float32")
+    print("Calculating standard deviations...")
+    img_dev = np.mean(images, axis=0) + (y * 3)
 
 except KeyboardInterrupt:
-	print("Program terminated.")
+    print("Program terminated.")
 
-total_time = 0
-for time in times:
-	total_time += time
+t1 = time.time()
 
-
-average_time = total_time / len(times)
-print("Average time per row: " + str(average_time) + "s")
-print("Total time to run: " + str((average_time * width) / 60) + "min")
+print("Total time to run: " + str(t1 - t0))
 print(img_dev)
+print(img_dev.shape)
 cv2.imwrite(save_folder + "Threshold.jpg", img_dev)
